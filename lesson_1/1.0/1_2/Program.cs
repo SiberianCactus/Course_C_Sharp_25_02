26c9eb6f5706077d04c59f401feb0761c10d9a55
SiberianCactus <kaktuz2101@mail.ru>
2023-03-01 19:58:56 +0700

    Task1_2

---
+++ lesson_1/1.0/1_2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+string day = Console.ReadLine()!;
+
+if (day == "1")
+{
+    Console.WriteLine("ПН");
+}
+else if (day == "2")
+{
+     Console.WriteLine("ВТ");
+}
+else if (day == "3")
+{
+   Console.WriteLine("СР");  
+}
+
+else if (day =="4")
+{
+   Console.WriteLine("ЧТ");  
+}
+
+else if (day == "5")
+{
+   Console.WriteLine("ПТ");  
+}
+
+else if (day == "6")
+{
+   Console.WriteLine("СБ");  
+}
+
+else if (day == "7")
+{
+   Console.WriteLine("ВС");  
+}
+
+else 
+{
+    Console.WriteLine("ERROR");
+}
+
+
+

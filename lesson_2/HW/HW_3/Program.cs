c7f4eb3c7ea6ccd18d69bf92505fe03695317892
SiberianCactus <kaktuz2101@mail.ru>
2023-03-04 21:35:34 +0700

    HomeWork_2_3

---
+++ lesson_2/HW/HW_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры 
+// нет.
+
+void func (string num)
+{
+    if (num.Length < 4 && num.Length > 2) 
+    Console.WriteLine(num[2]);
+    else if(num.Length < 3)
+Console.WriteLine("третьей цифры нет");
+else 
+Console.WriteLine(num[2]);
+}
+func("1234");

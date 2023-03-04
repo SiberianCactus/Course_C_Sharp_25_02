9c1c92a137225a43ca3fd33198fef0d5d5fe166e
SiberianCactus <kaktuz2101@mail.ru>
2023-03-04 20:55:34 +0700

    task 2_3

---
+++ lesson_2/2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿void exploer (int num)
+{
+    if (num % 7 == 0 && num % 23 == 0 )
+    Console.WriteLine("да");
+    else
+    Console.WriteLine("нет");
+
+}
+int num = int.Parse(Console.ReadLine()!);
+exploer(num);

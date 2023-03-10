cb0c7226ad9171562eb5fa340b99cdf37132aab2
SiberianCactus <kaktuz2101@mail.ru>
2023-03-11 00:20:13 +0700

    ClassWork_3_2

---
+++ lesson_3/3_2/Program.cs
@@ -0,0 +1,10 @@
+﻿double example (double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+}
+
+double a = double.Parse(Console.ReadLine()!);
+double s = double.Parse(Console.ReadLine()!);
+double d = double.Parse(Console.ReadLine()!);
+double h = double.Parse(Console.ReadLine()!);
+Console.WriteLine(example (a,s,d,h)); 

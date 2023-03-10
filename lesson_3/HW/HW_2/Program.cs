aca2dede06541db57c5127f5a538da1bbbe0a95e
SiberianCactus <kaktuz2101@mail.ru>
2023-03-11 00:44:36 +0700

    HomeWork_3_2

---
+++ lesson_3/HW/HW_2/Program.cs
@@ -0,0 +1,6 @@
+﻿double example (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
+}
+
+Console.WriteLine(example(7,-5,0,1,-1,9));

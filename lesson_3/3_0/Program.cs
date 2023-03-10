e1119bc2f465cc1724e1d04495c46dfa20a5f7d9
SiberianCactus <kaktuz2101@mail.ru>
2023-03-10 23:26:51 +0700

    ClassWork_3_0

---
+++ lesson_3/3_0/Program.cs
@@ -0,0 +1,18 @@
+﻿void quoters (int x, int y)
+{
+    if (x == 0 || y == 0)
+    Console.WriteLine ("ERROR!");
+    else if (x > 0 && y > 0)
+    Console.WriteLine ("I");
+       else if (x < 0 && y > 0)
+    Console.WriteLine ("II");
+       else if (x < 0 && y < 0)
+    Console.WriteLine ("III");
+       else if (x > 0 && y < 0)
+    Console.WriteLine ("IV");
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+
+quoters(num1,num2);

c517882281a599a49e23b7fc2ff3a5643ddf37ea
SiberianCactus <kaktuz2101@mail.ru>
2023-03-11 01:16:58 +0700

    HomeWork_3_3

---
+++ lesson_3/HW/HW_1/Program.cs
@@ -0,0 +1,21 @@
+﻿void num (string x) {
+    int j = 0;
+    for (int i = 1; i < x.Length-1; i++) //3
+    {
+        if (x[i-1] == x[x.Length - i ]) 
+        j = 1;
+        else 
+{
+        j = 0;
+        break;
+}
+    }
+        if (j == 1)
+    Console.WriteLine("Нет");
+    else 
+    {
+    Console.WriteLine("Да");
+    }
+}
+
+num("23432");

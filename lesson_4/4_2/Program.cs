c734323d9a76a3fffc6af96fc41949cc18839923
SiberianCactus <kaktuz2101@mail.ru>
2023-03-12 00:09:08 +0700

    ClassWork_4_2

---
+++ lesson_4/4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int sumNum(int num){
+    int sum = 1;
+    for (int i = 1; i <= num; i++){
+        sum *= i;
+    }
+return sum;
+}       
+
+int A = int.Parse(Console.ReadLine()!);
+int result = sumNum(A);
+Console.WriteLine(result);
+ 

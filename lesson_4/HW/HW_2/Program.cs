936d9435e78e784e5e04efd3615c9879baed0111
SiberianCactus <kaktuz2101@mail.ru>
2023-03-12 01:18:46 +0700

    HomeWork_4_1

---
+++ lesson_4/HW/HW_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int sumNum(int num)
+{
+    int all_sum = 0;
+    for (int i = 1; num > 0; i++)
+    {
+    // num = num % 10;
+    all_sum += num %10;
+    num = num / 10;
+    }
+return all_sum;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = sumNum(A);
+Console.WriteLine(result);
+ 

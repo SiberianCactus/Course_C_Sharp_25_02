36cd1f3f9105083ef438db85f8ceef7431292844
SiberianCactus <kaktuz2101@mail.ru>
2023-03-21 00:17:54 +0700

    ClassWork_6_3

---
+++ lesson_6/6_3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+void Fib (int num)
+{
+  int  a = 0;
+  int b = 1;
+    for (int i= 0; i < num; i++)
+    {
+          Console.Write(a + " " );
+          (a,b) = (b, a+b);
+    }
+}
+Console.WriteLine("Введите положительное число");
+int Input = int.Parse(Console.ReadLine()!);
+Fib(Input);

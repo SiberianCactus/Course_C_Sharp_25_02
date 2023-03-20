51103149d3a92a89026f6bcf14f02f847bf42f55
SiberianCactus <kaktuz2101@mail.ru>
2023-03-21 01:33:56 +0700

    HomeWork_6_1

---
+++ lesson_6/HW/HW_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int Pos ( string[] num)
+{
+    int num_1 = 0;
+
+for( int i = 0; i < num.Length; i++)
+    if(int.Parse(num[i]) > 0) num_1++;
+    return num_1;
+}
+Console.WriteLine("Введите числа через пробел");
+string[] tokens = Console.ReadLine()!.Split();
+Console.WriteLine("Количество положительных чисел: " + Pos(tokens));
+
+

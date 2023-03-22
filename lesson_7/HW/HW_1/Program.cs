0c559477d5f2547a3f793f6885632bca298862cb
SiberianCactus <kaktuz2101@mail.ru>
2023-03-23 00:51:27 +0700

    HomeWork_7_2

--- lesson_7/HW/HW_1/Program.cs
+++ lesson_7/HW/HW_1/Program.cs
@@ -1,4 +1,11 @@
-﻿void Print(double[,] arr)
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void Print(double[,] arr)
 {
     int row_size = arr.GetLength(0);
     int column_size = arr.GetLength(1);
---
+++ lesson_7/HW/HW_2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
+// этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)    
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to);          
+    return arr;
+}
+
+
+void Position(int[,] arr, int posRow, int posColumn)
+{
+     int Len_row = arr.GetLength(0);
+    int Len_column = arr.GetLength(1);
+
+if (posRow > Len_row || posColumn > Len_column)
+Console.Write("такого числа в массиве нет"); 
+
+else
+{
+    Console.Write(arr[posRow-1,posColumn-1]);
+}
+   
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+
+Position(mass,number1,number2);

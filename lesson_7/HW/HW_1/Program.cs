21850ef7e8ec393111adf626c4554d43dce12c80
SiberianCactus <kaktuz2101@mail.ru>
2023-03-23 00:13:30 +0700

    HomeWork_7_1

---
+++ lesson_7/HW/HW_1/Program.cs
@@ -0,0 +1,37 @@
+﻿void Print(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double [,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+  
+        arr[i, j] = Math.Round(from + new Random().NextDouble() * (to + from),2) ;
+
+
+    return arr;
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+

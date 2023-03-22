f4f3dae7d57cb5d16872f2bdbe9af650f4e7a80e
SiberianCactus <kaktuz2101@mail.ru>
2023-03-23 02:05:50 +0700

    HomeWork_7_3

---
+++ lesson_7/HW/HW_3/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)    
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to);          
+    return arr;
+}
+
+void AverNum(int[,] arr)
+	{
+        Console.Write($"Среднее арифметическое каждого столбца: ");
+        double sum1 = 0;
+        double aver = 0.0;
+        int row_size = arr.GetLength(0);
+        int column_size = arr.GetLength(1);
+
+    for (int j = 0; j < column_size; j++)
+        {
+         for (int i = 0; i < row_size; i++)
+          {
+         sum1 += arr[i,j];
+        }
+        aver = sum1 / row_size;
+         Console.Write($"{aver}; ");
+         sum1 = 0;
+    }
+	}
+	
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+AverNum(mass);

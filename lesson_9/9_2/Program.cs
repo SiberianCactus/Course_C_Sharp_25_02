99584cf5873a8a2560ffb74644f63334cd6a2f2e
SiberianCactus <kaktuz2101@mail.ru>
2023-03-29 02:44:14 +0700

    ClassWork_9_2

---
+++ lesson_9/9_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int ValueSet(int N)
+{
+    if (N == 0)
+        return 0;
+    return ValueSet(N / 10) + N % 10;
+}
+
+Console.WriteLine("Введите натуральнsе числа");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(ValueSet(num));

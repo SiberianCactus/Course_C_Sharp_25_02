1bcf65f69596fa92e16edc13e00fd5ea0ba3f53c
SiberianCactus <kaktuz2101@mail.ru>
2023-03-29 00:43:22 +0700

    HomeWork_8_1

---
+++ lesson_8/HW/HW_2/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int[] SumNum    (int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    int[] all_nums = new int[row_size];
+
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            all_nums[i] += arr[i,j];
+       
+    }
+    return all_nums;
+}
+
+void FindMin(int[] arr)
+{
+    int min_num = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i]<arr[min_num]) min_num = i;
+    }
+    Console.WriteLine($"Sum of elements: {arr[min_num]}, line: {min_num +1}");
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+int[] sum = SumNum(mass);
+FindMin(sum);

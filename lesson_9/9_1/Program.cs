e6ee4bb76c241270d6aa78acb8accf0aaaae32e1
SiberianCactus <kaktuz2101@mail.ru>
2023-03-29 02:42:20 +0700

    ClassWork_9_1

---
+++ lesson_9/9_1/Program.cs
@@ -0,0 +1,11 @@
+﻿void ValuesSet(int M, int N)
+	{
+	    if (N < M) return;
+	    ValuesSet(M , N- 1);
+	    Console.Write($"{N} ");
+	}
+
+Console.WriteLine("Введите натуральнsе числа");
+int num = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+ValuesSet(num, num2);
